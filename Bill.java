95dfe7f4932c1b802a3f33bd438f4c7dab80b5c5
e1982b <e1982b@gmail.com>
2022-10-20 11:10:40 +0200

    My First Project

---
+++ Bill.java
@@ -0,0 +1,8 @@
+public class Homework191022 {
+
+    public static void main(String[] args){
+
+        System.out.println("L O V E, T A Y  P O D C A S T\n------------------------------\nITEM ONE\t\t\t\t$23.50\nITEM TWO\t\t\t\t$3.21\nITEM THREE\t\t\t\t$67.98\nITEM FOUR\t\t\t\t$12.39\n\n-------------------------------\n SUBTOTAL\t\t\t\t$107.08\n LOYALTY LISTENER\t\t-$5.00\n-------------------------------\n TOTAL AMOUNT\t\t\t$102.08\n CASH\t\t\t\t\t$105.00\n CHANGE\t\t\t\t\t$2.92\n-------------------------------\n    THANK YOU FOR LISTENING!  \n-------------------------------");
+    }
+
+}
